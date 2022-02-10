ad948eba07893ccb0b5a5208bfe8be6ba039d0a2
AleksandraKlua <alex.klua@gmail.com>
2022-02-10 23:15:58 +0300

    feat: project settings

---
+++ src/main/java/com/sut/personalmedicalcartbackend/PersonalMedicalCartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sut.personalmedicalcartbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalMedicalCartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalMedicalCartBackendApplication.class, args);
+	}
+
+}
